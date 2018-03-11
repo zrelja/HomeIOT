a399eec2a6b21c5281cd87de6c2e204386d17e3b
Unknown <zvonimir_relja@yahoo.com>
2018-03-11 13:50:58 +0100

    esp32 hello world

---
+++ esp32-dipl/src/main.cpp
@@ -0,0 +1,24 @@
+
+#include <Arduino.h>
+
+#define LED_BUILTIN 2
+
+void setup()
+{
+  // initialize LED digital pin as an output.
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);
+  // wait for a second
+  delay(1000);
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("Blink");
+   // wait for a second
+  delay(1000);
+}
