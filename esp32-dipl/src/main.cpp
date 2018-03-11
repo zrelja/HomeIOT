969be21cf26377cee200f9b3335d60155bd7d9dd
Unknown <zvonimir_relja@yahoo.com>
2018-03-11 18:21:42 +0100

    wifi scan implementation
    Able to scan available wifi networks

--- esp32-dipl/src/main.cpp
+++ esp32-dipl/src/main.cpp
@@ -1,6 +1,6 @@
 
 #include <Arduino.h>
-
+#include "wifiScan.h"
 #define LED_BUILTIN 2
 
 void setup()
@@ -8,10 +8,12 @@ void setup()
   // initialize LED digital pin as an output.
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
+  wifiScan();
 }
 
 void loop()
 {
+
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
   // wait for a second
@@ -19,6 +21,7 @@ void loop()
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
   Serial.println("Blink");
+
    // wait for a second
   delay(1000);
 }
