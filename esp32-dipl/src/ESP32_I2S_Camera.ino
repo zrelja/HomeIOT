286a04d3796b2ffac37ef20621ffecafd59be287
Unknown <zvonimir_relja@yahoo.com>
2018-03-17 23:23:46 +0100

    camera working
    http://bitluni.net/esp32-i2s-camera-ov7670/

---
+++ esp32-dipl/src/ESP32_I2S_Camera.ino
@@ -0,0 +1,132 @@
+#include "OV7670.h"
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library
+
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <WiFiClient.h>
+#include "BMP.h"
+
+const int SIOD = 25; //SDA
+const int SIOC = 23; //SCL
+
+const int VSYNC = 22;
+const int HREF = 26;
+
+const int XCLK = 27;
+const int PCLK = 21;
+
+const int D0 = 35;
+const int D1 = 17;
+const int D2 = 34;
+const int D3 = 5;
+const int D4 = 39;
+const int D5 = 18;
+const int D6 = 36;
+const int D7 = 19;
+
+//const int TFT_DC = 2;
+//const int TFT_CS = 5;
+//DIN <- MOSI 23
+//CLK <- SCK 18
+
+#define ssid1        "zvone"
+#define password1    "hajduk1950"
+//#define ssid2        ""
+//#define password2    ""
+
+//Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, 0/*no reset*/);
+OV7670 *camera;
+
+WiFiMulti wifiMulti;
+WiFiServer server(80);
+
+unsigned char bmpHeader[BMP::headerSize];
+
+void serve()
+{
+    Serial.println("serviram");
+  WiFiClient client = server.available();
+  if (client)
+  {
+    Serial.println("New Client.");
+    String currentLine = "";
+    while (client.connected())
+    {
+      if (client.available())
+      {
+        char c = client.read();
+        Serial.write(c);
+        if (c == '\n')
+        {
+          if (currentLine.length() == 0)
+          {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println();
+            client.print(
+              "<style>body{margin: 0}\nimg{height: 100%; width: auto}</style>"
+              "<img id='a' src='/camera' onload='this.style.display=\"initial\"; var b = document.getElementById(\"b\"); b.style.display=\"none\"; b.src=\"camera?\"+Date.now(); '>"
+              "<img id='b' style='display: none' src='/camera' onload='this.style.display=\"initial\"; var a = document.getElementById(\"a\"); a.style.display=\"none\"; a.src=\"camera?\"+Date.now(); '>");
+            client.println();
+            break;
+          }
+          else
+          {
+            currentLine = "";
+          }
+        }
+        else if (c != '\r')
+        {
+          currentLine += c;
+        }
+
+        if(currentLine.endsWith("GET /camera"))
+        {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:image/bmp");
+            client.println();
+
+            for(int i = 0; i < BMP::headerSize; i++)
+               client.write(bmpHeader[i]);
+            for(int i = 0; i < camera->xres * camera->yres * 2; i++)
+               client.write(camera->frame[i]);
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    Serial.println("Client Disconnected.");
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  wifiMulti.addAP(ssid1, password1);
+  //wifiMulti.addAP(ssid2, password2);
+  Serial.println("Connecting Wifi...");
+  if(wifiMulti.run() == WL_CONNECTED) {
+      Serial.println("");
+      Serial.println("WiFi connected");
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+  }
+
+  camera = new OV7670(OV7670::Mode::QQVGA_RGB565, SIOD, SIOC, VSYNC, HREF, XCLK, PCLK, D0, D1, D2, D3, D4, D5, D6, D7);
+  BMP::construct16BitHeader(bmpHeader, camera->xres, camera->yres);
+  server.begin();
+    Serial.println("server begin");
+}
+
+
+void loop()
+{
+    Serial.println("loop");
+  camera->oneFrame();
+  Serial.println("kamera slikana");
+  serve();
+
+}
